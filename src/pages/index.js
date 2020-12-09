7f1780588389012aaf1387fa1ef8d369096b5ddc
Interpause <42513874+Interpause@users.noreply.github.com>
2020-12-09 03:40:52 +0000

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,6 +24,7 @@ function Home() {
             Help contribute to the community by donating any supplies that you
             can spare to those who need them the most! (especially with what's
             going on in the world today)
+            test
           </p>
         </div>
       </section>
